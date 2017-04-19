17eef0bffe9fa9f4cf3f9d0f5e0251616262bb20
PangeaBogota <Pangeabogota@hotmail.com>
2017-04-19 08:29:11 -0500

    qweqwe
    qweqwe

--- controller/configuracionController.js
+++ controller/configuracionController.js
@@ -1,18 +1,26 @@
 app_angular.controller("configController",['Conexion','$scope','$route',function (Conexion,$scope,$route) {
-	$scope.MAESTROS_SINCRONIZACION=JSON.parse(window.localStorage.getItem("MAESTROS_SINCRONIZACION"));
-	if ($scope.MAESTROS_SINCRONIZACION==null || $scope.MAESTROS_SINCRONIZACION==undefined || $scope.MAESTROS_SINCRONIZACION==NaN) 
+	try
 	{
-		$scope.MAESTROS_SINCRONIZACION={
-			pedido:true,
-			tercero:true,
-			actividad:true,
-			item:true
+		$scope.MAESTROS_SINCRONIZACION=JSON.parse(window.localStorage.getItem("MAESTROS_SINCRONIZACION"));
+		if ($scope.MAESTROS_SINCRONIZACION==null || $scope.MAESTROS_SINCRONIZACION==undefined || $scope.MAESTROS_SINCRONIZACION==NaN) 
+		{
+			$scope.MAESTROS_SINCRONIZACION={
+				pedido:true,
+				tercero:true,
+				actividad:true,
+				item:true
+			}
+		}
+		$scope.Guardar=function()
+		{
+			localStorage.removeItem('MAESTROS_SINCRONIZACION');
+			localStorage.setItem('MAESTROS_SINCRONIZACION', JSON.stringify($scope.MAESTROS_SINCRONIZACION)); 
+			Mensajes('Guardado Correctamente','success');
 		}
 	}
-	$scope.Guardar=function()
+	catch(error)
 	{
-		localStorage.removeItem('MAESTROS_SINCRONIZACION');
-		localStorage.setItem('MAESTROS_SINCRONIZACION', JSON.stringify($scope.MAESTROS_SINCRONIZACION)); 
-		Mensajes('Guardado Correctamente','success');
+		console.log('error')
 	}
+	
 }]);
